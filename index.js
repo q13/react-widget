c9c576640f29b38f0c8eb34043a277012856c6e3
q13 <qsimon13@gmail.com>
2015-12-11 14:14:38 +0800

    Update index.

--- index.js
+++ index.js
@@ -3,7 +3,11 @@
  */
 import Grid from "./src/component/grid";
 import Validation from "./src/component/validation";
+import Calendar from "./src/component/calendar";
+import DateInput from "./src/component/form/DateInput.js";
 export {
     Grid,
-    Validation
+    Validation,
+    Calendar,
+    DateInput
 };
