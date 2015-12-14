d79263a330a858574c3cae015bdc23a6db0f3514
q13 <qsimon13@gmail.com>
2015-12-14 13:49:29 +0800

    Add Modal in index.js.

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ import Grid from "./src/component/grid";
 import Validation from "./src/component/validation";
 import Calendar from "./src/component/calendar";
 import DateInput from "./src/component/form/DateInput.js";
+import Modal from "./src/component/modal";
 export {
     Grid,
     Validation,
     Calendar,
-    DateInput
+    DateInput,
+    Modal
 };
