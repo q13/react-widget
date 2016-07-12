51b4f3e9992b00e1539e7dc65be9d154e7456005
13 Q <13 Q>
2016-07-12 14:30:03 +0800

    v1.8.1

--- src/component/tree/index.js
+++ src/component/tree/index.js
@@ -1,6 +1,6 @@
 /**
 * @Date:   2016-06-17T16:39:09+08:00
-* @Last modified time: 2016-07-11T14:15:07+08:00
+* @Last modified time: 2016-07-12T14:15:48+08:00
 */
 
 /**
@@ -24,7 +24,16 @@ class Tree extends Widget {
     };
   }
   componentDidMount() {
+    const props = this.props;
     this.renderTid = null;  //用于延时加载
+    //判定初始状态下是否需要展开节点
+    if (props.unfoldMode === 'all') {
+      props.options.forEach((itemData) => {
+        this.lazyUnfoldOption(itemData, () => {
+          props.onOptionsChange([].concat(props.options)); //反射
+        }, true);
+      });
+    }
   }
   componentWillReceiveProps(nextProps) {
     this.adaptOptionStatus(nextProps);
@@ -35,7 +44,9 @@ class Tree extends Widget {
   }
   adaptOptionStatus(props) {
     //处理半勾选状态
-    loop(props.options);
+    if (props.checkMode !== 'none') {
+      loop(props.options);
+    }
 
     function loop(options, forceCheck) {
       options.forEach((itemData) => {
@@ -78,12 +89,85 @@ class Tree extends Widget {
     }
   }
   /**
+   * 获取selected或者checked状态
+   * @param  {[type]} statusType [description]
+   * @param  {[type]} options    [description]
+   * @return {[type]}            [description]
+   */
+  getStatusRecord(statusType, options) {
+    var record = [];
+    loop(options);
+    function loop(options) {
+      options.forEach((itemData) => {
+        if(itemData[statusType + 'Status'] === statusType) {
+          record.push(itemData.value);
+        }
+        if (itemData.children && itemData.children.length) {
+          loop(itemData.children);
+        }
+      });
+    }
+    return record;
+  }
+  lazyUnfoldOption(option, callback, isDeepin) {
+    const self = this;
+    option.foldStatus = 'unfold';
+    if (option.children && option.children.length) {
+      let loop = function (options) {
+        if (options.length > 50) {
+          //let totalChildren = option.children;
+          let counts = Math.ceil(options.length / 50.0);
+          let i = 0;
+          //option.children = [];
+          let unfoldLoop = function () {
+            self.renderTid = setTimeout(() => {
+              //option.children = option.children.concat(totalChildren.slice(i * 100, 100));
+              options.slice(i * 50, (i + 1) * 50).forEach((itemData) => {
+                itemData.rendered = true;
+              });
+              callback();
+              i++;
+              if (i < counts) {
+                unfoldLoop();
+              } else {
+                if (isDeepin) { //判定是否做深层次展开
+                  options.forEach((itemData) => {
+                    itemData.foldStatus = 'unfold';
+                    if (itemData.children && itemData.children.length) {
+                      loop(itemData.children);
+                    }
+                  });
+                }
+              }
+            }, 0);
+          };
+          unfoldLoop();
+        } else {
+          options.forEach((itemData) => {
+            itemData.rendered = true;
+          });
+          callback();
+          if (isDeepin) { //判定是否做深层次展开
+            options.forEach((itemData) => {
+              itemData.foldStatus = 'unfold';
+              if (itemData.children && itemData.children.length) {
+                loop(itemData.children);
+              }
+            });
+          }
+        }
+      };
+      loop(option.children);
+    }
+  }
+  /**
    * Node check handler
    * @param  {[type]} node [description]
    * @return {[type]}      [description]
    */
   handleOptionCheck(option) {
     const props = this.props;
+    const state = this.state;
     let options = [].concat(props.options);
     option.checkedStatus = (option.checkedStatus === 'checked' ? 'unchecked' : 'checked');
     //反选其它node
@@ -92,6 +176,15 @@ class Tree extends Widget {
     }
     //同步状态
     syncStatusLoop(options);
+    props.onOptionsChange(options); //反射
+    let newCheckedStatusRecord = this.getStatusRecord('checked', options);
+    if (JSON.stringify(state.checkedStatusRecord) !== JSON.stringify(newCheckedStatusRecord)) {
+      this.setState({
+        checkedStatusRecord: newCheckedStatusRecord
+      });
+      props.onCheckedChange(option, newCheckedStatusRecord);
+    }
+
     //向上向下控制
     function uncheckOtherNode(options) {
       options.forEach((itemData) => {
@@ -142,10 +235,10 @@ class Tree extends Widget {
         }
       });
     }
-    props.onOptionsChange(options); //反射
   }
   handleOptionSelect(option) {
     const props = this.props;
+    const state = this.state;
     let options = [].concat(props.options);
     option.selectedStatus = (option.selectedStatus === 'selected' ? 'unselected' : 'selected');
     //反选其它node
@@ -163,6 +256,13 @@ class Tree extends Widget {
       });
     }
     props.onOptionsChange(options); //反射
+    let newSelectedStatusRecord = this.getStatusRecord('selected', options);
+    if (JSON.stringify(state.selectedStatusRecord) !== JSON.stringify(newSelectedStatusRecord)) {
+      this.setState({
+        selectedStatusRecord: newSelectedStatusRecord
+      });
+      props.onSelectedChange(option, newSelectedStatusRecord);
+    }
   }
   handleOptionFold(option) {
     const props = this.props;
@@ -173,31 +273,9 @@ class Tree extends Widget {
       // props.onOptionsChange([].concat(options)); //反射
       // return;
     if (option.foldStatus === 'unfold') { //分时渲染调优性能
-      if (option.children.length > 50) {
-        //let totalChildren = option.children;
-        let counts = Math.ceil(option.children.length / 50.0);
-        let i = 0;
-        //option.children = [];
-        let loop = function () {
-          self.renderTid = setTimeout(() => {
-            //option.children = option.children.concat(totalChildren.slice(i * 100, 100));
-            option.children.slice(i * 50, (i + 1) * 50).forEach((itemData) => {
-              itemData.rendered = true;
-            });
-            props.onOptionsChange([].concat(options)); //反射
-            i++;
-            if (i < counts) {
-              loop();
-            }
-          }, 0);
-        };
-        loop();
-      } else {
-        option.children.forEach((itemData) => {
-          itemData.rendered = true;
-        });
+      this.lazyUnfoldOption(option, () => {
         props.onOptionsChange([].concat(options)); //反射
-      }
+      });
     } else {
       option.children.forEach((itemData) => {
         itemData.rendered = false;
@@ -275,6 +353,7 @@ Tree.propTypes = {
   className: React.PropTypes.string,
   checkMode: React.PropTypes.string,
   selectMode: React.PropTypes.string,
+  unfoldMode: React.PropTypes.string,
   options: React.PropTypes.array,
   onOptionsChange: React.PropTypes.func,
   onCheckedChange: React.PropTypes.func,
@@ -283,8 +362,9 @@ Tree.propTypes = {
 Tree.defaultProps = {
   prefixCls: 'ui-tree',
   className: '',
-  checkMode: 'multi', //multi or single
+  checkMode: 'multi', //multi or single or none
   selectMode: 'single', //multi or single
+  unfoldMode: 'none', // none or all
   options: [],  //{value, text, checkedStatus, checkType, selectedStatus} checkedStatus取值checked，halfChecked,unchecked(默认) checkType取值none(默认),checkbox
   onOptionsChange: () => {},
   onCheckedChange: () => {},
