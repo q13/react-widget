79a0de553ca2a6188e23669b65877628ae7d3ecb
13 <qsimon13@gmail.com>
2016-08-10 12:10:39 +0800

    v2.0.9

--- src/component/grid/GridRow.js
+++ src/component/grid/GridRow.js
@@ -1,59 +1,67 @@
 import React from 'react';
 import {
-    Widget
+  Widget
 } from '../component.js';
 
 class GridRow extends Widget {
-    componentWillUnmount() {
-        this.props.onBeforeDestroy(this.props.record);
-    }
-    render() {
-        const props = this.props;
-        const prefixCls = props.prefixCls;
-        const columns = props.columns;
-        const record = props.record;
-        const index = props.index;
-        const cells = [];
+  componentWillUnmount() {
+    this.props.onBeforeDestroy(this.props.record);
+  }
 
-        for (let i = 0; i < columns.length; i++) {
-            const col = columns[i];
-            const colCls = col.className || '';
-            const renderer = col.renderer;
-            let text = record[col.dataIndex];
+  render() {
+    const props = this.props;
+    const prefixCls = props.prefixCls;
+    const columns = props.columns;
+    const record = props.record;
+    const index = props.index;
+    const cells = [];
 
-            let tdProps;
-            let colSpan;
-            let rowSpan;
-            let notRender = false;
+    for (let i = 0; i < columns.length; i++) {
+      const col = columns[i];
+      const colCls = col.className || '';
+      const renderer = col.renderer;
+      let width = col.width || 'auto';
+      let text = record[col.dataIndex];
 
-            if (renderer) {
-                text = renderer(text, record, index) || {};
-                tdProps = text.props || {};
-                if (!React.isValidElement(text) && 'content' in text) {
-                    text = text.content;
-                }
-                rowSpan = tdProps.rowSpan;
-                colSpan = tdProps.colSpan;
-            }
-            if (rowSpan === 0 || colSpan === 0) {
-                notRender = true;
-            }
-            if (!notRender) {
-                cells.push(<td key={i} colSpan={colSpan} rowSpan={rowSpan} className={`${colCls}`}>
-                    {text}
-                </td>);
-            }
+      let tdProps;
+      let colSpan;
+      let rowSpan;
+      let notRender = false;
+      if (/^[0-9]*$/.test(width)) {
+        width = width + 'px';
+      }
+
+      if (renderer) {
+        text = renderer(text, record, index) || {};
+        tdProps = text.props || {};
+        if (!React.isValidElement(text) && 'content' in text) {
+          text = text.content;
         }
-        return (<tr className={`${prefixCls} ${props.className || ''}`}>{cells}</tr>);
+        rowSpan = tdProps.rowSpan;
+        colSpan = tdProps.colSpan;
+      }
+      if (rowSpan === 0 || colSpan === 0) {
+        notRender = true;
+      }
+      if (!notRender) {
+        cells.push(<td key={i} colSpan={colSpan} rowSpan={rowSpan} className={`${colCls}`} style={{
+          width: width
+        }}>
+          {text}
+        </td>);
+      }
     }
+    return (<tr className={`${prefixCls} ${props.className || ''}`}>{cells}</tr>);
+  }
 }
 GridRow.propTypes = {
-    onBeforeDestroy: React.PropTypes.func,
-    record: React.PropTypes.object,
-    prefixCls: React.PropTypes.string,
+  onBeforeDestroy: React.PropTypes.func,
+  record: React.PropTypes.object,
+  prefixCls: React.PropTypes.string,
 };
 GridRow.defaultProps = {
-    onBeforeDestroy: () => {}
+  onBeforeDestroy: () => {
+  }
 };
 
 export default GridRow;
